a2d14e5a0eedbe5132f06c72d73a7b7ba6a495a9
TarannumParwin <105907591+TarannumParwin@users.noreply.github.com>
2022-09-12 14:15:26 +0530

    Update Header.js

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -6,7 +6,24 @@ const Header = () => {
       <div className="ui container center">
 
         <h2>Tara Shop</h2>
+    <div>
+    <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">HOME</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
+</nav>
+    </div>
+  </div>
+    
     </div>
   );
 };
